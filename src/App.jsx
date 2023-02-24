61b3ba54ee55cc4e0461f1bcef4218563fc84b92
bohjaangles <bohjaangles@gmail.com>
2023-02-24 14:29:50 -0600

    edit css for components to be positioned properly in browser

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './styles/App.scss'
 import ProjectList from './components/ProjectList'
 import Header from './components/Header'
 import NavBar from './components/NavBar'
-import SideBar from './components/SideBar'
 import Footer from './components/Footer'
 
 function App() {
@@ -13,10 +12,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Header />
-      <div className="body-ody">
-        <SideBar />
-        <ProjectList />
-      </div>
+      <ProjectList />
       <Footer />
     </div>
   )
