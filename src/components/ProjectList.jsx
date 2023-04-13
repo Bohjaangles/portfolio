a4afe0511fa95efaa3e72cc2e0fa66527ace1b85
Russel Mueller <bohjaangles@gmail.com>
2023-04-13 11:36:10 -0600

    add links to project list items, format project list items, add github link to project list items.

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ProjectListItem from './ProjectListItem'
-import { Canvas, useFrame } from '@react-three/fiber'
 
 const projArray = [
-  {key: 1, name: 'ThreeJs-Demo', descritption: 'Short and sweet interactive environment showcasing skills using three js and react three fiber', url: 'test url 1'}, 
-  {key: 2, name: 'test 2', descritption: 'Lorem Hipsum good ole badburgers from the library of ipsum at lorem. Featuring the lightly hatted and fantastically feathered', url: 'test url 2'},
-  {key: 3, name: 'test 3', descritption: 'Lorem Hipsum good ole badburgers from the library of ipsum at lorem. Featuring the lightly hatted and fantastically feathered', url: 'test url 3'},
-  {key: 4, name: 'test 4', descritption: 'Lorem Hipsum good ole badburgers from the library of ipsum at lorem. Featuring the lightly hatted and fantastically feathered', url: 'test url 4'}
+  {key: 1, name: 'ThreeJs-Demo', description: 'Short and sweet interactive environment showcasing skills using three js and react three fiber. In the live link, drag around the laptop. On the screen, try scrolling in or right and left click draging the screen around, the shapes are itneractable too!', url: 'https://github.com/Bohjaangles/ThreeJS-Demo', liveLink: 'https://threejs-demo-63mch1luy-bohjaangles.vercel.app/'}, 
+  {key: 2, name: 'test 2', description: 'Lorem Hipsum good ole badburgers from the library of ipsum at lorem. Featuring the lightly hatted and fantastically feathered', url: 'test url 2', liveLink: 'https://youtu.be/wWywwmZysC0'},
+  {key: 3, name: 'test 3', description: 'Lorem Hipsum good ole badburgers from the library of ipsum at lorem. Featuring the lightly hatted and fantastically feathered', url: 'test url 3', liveLink: 'https://www.youtube.com/watch?v=wWywwmZysC0&ab_channel=RusselMueller'},
+  {key: 4, name: 'test 4', description: 'Lorem Hipsum good ole badburgers from the library of ipsum at lorem. Featuring the lightly hatted and fantastically feathered', url: 'test url 4', liveLink: 'https://www.youtube.com/watch?v=wWywwmZysC0&ab_channel=RusselMueller'}
 ]
 
 export default function ProjectList() {
@@ -19,8 +18,9 @@ export default function ProjectList() {
             className='projListItem'
             key={proj.key}
             name={proj.name}
-            description={proj.descritption}
+            description={proj.description}
             url={proj.url}
+            liveLink={proj.liveLink}
           />
         )
       })}
--- src/components/ProjectListItem.jsx
+++ src/components/ProjectListItem.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import '../styles/projectListItem.scss'
 
-export default function ProjectListItem({className, name, description, url}) {
+export default function ProjectListItem({className, name, description, url, liveLink}) {
 
   return (
     <div className={className}>
       <div className="card-wrapper">
         <div className="text-details">
           <h1>{name}</h1>
+          <div className="githubLink">
+            <a href={url} style={{color: 'aqua'}}>Github repo link</a>
+          </div>
           <div className="text-wrapper">
             <p>{description}</p>
+          </div>
+          <div className="liveLink">
+            <iframe width='560' height='315' src={liveLink} allowFullScreen></iframe>
           </div>
         </div>
       </div>
