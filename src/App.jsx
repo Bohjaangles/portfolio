e833116ba2729f9f1ea8ddd03eaeaa9369aa0936
Russel Mueller <bohjaangles@gmail.com>
2023-04-14 15:24:08 -0600

    add outline and section for bio

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ProjectList from './components/ProjectList'
 import Header from './components/Header'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
+import AboutMe from './components/AboutMe'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <NavBar />
       <Header />
       <ProjectList />
+      <AboutMe />
       <Footer />
     </div>
   )
