9125575014254fdc31e2f12ba6e94e14c08f47fd
Russel Mueller <bohjaangles@gmail.com>
2023-04-15 17:18:46 -0600

    remove header component, move aboutme component to top, begin rocess of formatiing aboutme component

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../styles/aboutMe.scss'
+import headshot from '../assets/headshot.png'
 
 export default function AboutMe() {
   return (
@@ -7,13 +8,15 @@ export default function AboutMe() {
       <div className="AMTitle">
         <h2>Russel Mueller</h2>
       </div>
-      <div className="AMImage">
-        <img src={null}></img>
-      </div>
-      <div className="AMText">
-        <p>
-          Hi there, I am a junior web developer from Canada. My training is in full stack web developnment, although I have interests in the 3d and animation space and enjoy working with Unity and threeJS. I was drawn into development because of my love of strategy and problem solving, and my desire to improve the world around me in a way that is ultimately scalable. As a coder, my core skills and strengths lie in my attention to detail, actual enjoymebnt of the development process and my ability to quickly learn new languages and frameworks on an as needed basis as demonstrated multiple times. In my personal life I am often found rock climbing in a gym during the week or at the crag on long weekends and I am an avid reader of science fiction. 
-        </p>
+      <div className="largeCard">
+        <div className="AMImage">
+          <img src={headshot} height={250} width={250}></img>
+        </div>
+        <div className="AMText">
+          <p>
+            Hi there, I am a junior web developer from Canada. My training is in full stack web developnment, although I have interests in the 3d and animation space and enjoy working with Unity and threeJS. I was drawn into development because of my love of strategy and problem solving, and my desire to improve the world around me in a way that is ultimately scalable. As a coder, my core skills and strengths lie in my attention to detail, actual enjoymebnt of the development process and my ability to quickly learn new languages and frameworks on an as needed basis as demonstrated multiple times. In my personal life I am often found rock climbing in a gym during the week or at the crag on long weekends and I am an avid reader of science fiction. 
+          </p>
+        </div>
       </div>
     </div>
   )
